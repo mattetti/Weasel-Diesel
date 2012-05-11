7fa7e35da3f3b48c3a4f299275af733c27fa96c1
Matt Aimonetti <mattaimonetti@gmail.com>
2012-05-11 20:38:40 +0200

    reworked a spec to make it pass on travis

--- spec/wsdsl_sinatra_ext_spec.rb
+++ spec/wsdsl_sinatra_ext_spec.rb
@@ -4,20 +4,30 @@ require File.expand_path("./../lib/framework_ext/sinatra.rb", File.dirname(__FIL
 WeaselDiesel.send(:include, WeaselDieselSinatraExtension)
 
 describe "Hello World example" do
-    require "hello_world_service"
-    require "hello_world_controller"
+
+    before :all do
+      @original_use_controller_dispatch = WeaselDiesel.use_controller_dispatch
+      WeaselDiesel.use_controller_dispatch = true
+      @original_services = WSList.all.dup
+      WSList.all.clear
+      require "hello_world_service"
+      require "hello_world_controller"
+      @service = WSList.all.find{|s| s.url == 'hello_world.xml'}
+      @service.should_not be_nil
+      @service.load_sinatra_route
+    end
+
+    after :all do
+      WeaselDiesel.use_controller_dispatch = @original_use_controller_dispatch
+      WSList.all.replace @original_services
+    end
 
     def app
       Sinatra::Application
     end
 
-    before(:all) do
-      service = WSList.all.find{|s| s.url == 'hello_world.xml'}
-      service.should_not be_nil
-      service.load_sinatra_route
-    end
-
     it "should dispatch the hello world service properly" do
+      @service.controller_name.should == "HelloWorldController"
       get "/hello_world.xml"
       last_response.body.should include("Hello World")
     end
